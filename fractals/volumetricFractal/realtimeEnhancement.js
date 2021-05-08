5aa39949d6725563305a0486cd95018511363107
Josh Greig <josh.greig@gmail.com>
2021-05-08 19:31:25 -0400

    volumetricFractal: moved to fractals directory

---
+++ fractals/volumetricFractal/realtimeEnhancement.js
@@ -0,0 +1,11 @@
+/*
+Responsible for improving quality of still image when user stops 
+changing anything that should change how the image is rendered.
+*/
+class RealtimeEnhancement {
+	constructor() {
+		
+	}
+
+
+}
