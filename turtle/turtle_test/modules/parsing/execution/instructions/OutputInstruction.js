991790890b32d6ba0e2e55914b6c778491077d3a
Josh Greig <josh.greig@gmail.com>
2024-01-28 03:39:55 -0500

    turtle: added turtle_test

---
+++ turtle/turtle_test/modules/parsing/execution/instructions/OutputInstruction.js
@@ -0,0 +1,33 @@
+import { LogoInstruction } from './LogoInstruction.js';
+export class OutputInstruction extends LogoInstruction {
+	static _name = 'output';
+
+	constructor(parseTreeToken) {
+		super(true, parseTreeToken);
+	}
+
+	static createFromDTO(dto, token) {
+		return new OutputInstruction(token);
+	}
+
+	execute(context) {
+		OutputInstruction.executeWithOutputValue(context, context.valueStack.pop());
+	}
+
+	static executeWithOutputValue(context, returnValue) {
+		const proc = context.procedureStack.pop();
+		const len = context.valueStack.length;
+		context.instructionIndex = proc.returnInstructionIndex;
+		context.repcountStack.length = proc.repcountStackHeight;
+		context.forcountStack.length = proc.forStackHeight;
+		context.valueStack.length = proc.valueStackHeight;
+		context.valueStack.push(returnValue);
+	}
+
+	toDTO() {
+		return {
+			'name': OutputInstruction._name
+		};
+	}
+
+};
