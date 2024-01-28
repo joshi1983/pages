2ce1ac75809e8bd2d57f7429fb7234346e52a028
Josh Greig <josh.greig@gmail.com>
2024-01-28 03:53:51 -0500

    turtle: improved turtle_test

--- turtle/turtle_test/modules/parsing/parse-tree-analysis/tip-generators/logAllTips.js
+++ turtle/turtle_test/modules/parsing/parse-tree-analysis/tip-generators/logAllTips.js
@@ -1,16 +1,2 @@
-import { getAnimationTips } from './getAnimationTips.js';
-import { getTipsForJumping } from './getTipsForJumping.js';
-import { getTipsForProcOrCommandName } from './getTipsForProcOrCommandName.js';
-import { ParseTreeToken } from '../../ParseTreeToken.js';
-
-const tipGenerators = [
-getAnimationTips,
-getTipsForJumping,
-getTipsForProcOrCommandName
-];
-
 export function logAllTips(cachedParseTree, parseLogger) {
-	tipGenerators.forEach(function(tipGenerator) {
-		tipGenerator(cachedParseTree, parseLogger);
-	});
 };
