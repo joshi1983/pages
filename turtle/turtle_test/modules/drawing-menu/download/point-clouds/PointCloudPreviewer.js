e9bd73b0be183fad8f280c290d9600b2de108740
Josh Greig <josh.greig@gmail.com>
2024-01-28 03:39:55 -0500

    turtle: added turtle_test

---
+++ turtle/turtle_test/modules/drawing-menu/download/point-clouds/PointCloudPreviewer.js
@@ -0,0 +1,102 @@
+import { Colour } from '../../../Colour.js';
+import { drawPointCloud } from './drawPointCloud.js';
+import { PerspectiveTransformer } from '../../../drawing/drawers/transformers/PerspectiveTransformer.js';
+import { pointsToBackgroundColour } from './pointsToBackgroundColour.js';
+import { RotatingTransformer } from './RotatingTransformer.js';
+import { sortPointCloudPoints } from './sortPointCloudPoints.js';
+import { updatePointCentre } from './updatePointCentre.js';
+import { Vector3D } from '../../../drawing/vector/Vector3D.js';
+await Colour.asyncInit();
+
+export class PointCloudPreviewer {
+	constructor(container, points) {
+		this.root = container;
+		this.backgroundColour = new Colour("white");
+		this.canvas = document.createElement('canvas');
+		container.appendChild(this.canvas);
+		this.perspectiveTransformer = new PerspectiveTransformer(100, 100);
+		const outer = this;
+		this._resized = function() {outer.updateDimensions();};
+		window.addEventListener('resize', this._resized);
+		this.t = setInterval(function() {outer.redraw();}, 30);
+		if (points !== undefined)
+			this.setPoints(points);
+		this.rotatingTransformer = new RotatingTransformer(this);
+	}
+
+	dispose() {
+		clearInterval(this.t);
+		window.removeEventListener('resize', this._resized);
+		this.rotatingTransformer.dispose();
+		this.root.removeChild(this.canvas);
+		this.root = undefined;
+	}
+
+	multiplyScaleBy(scaleFactor) {
+		this.perspectiveTransformer.multiplyScaleBy(scaleFactor);
+		this.redrawNeeded();
+	}
+
+	nudgeIn() {
+		this.perspectiveTransformer.multiplyScaleBy(1.01);
+		this.redrawNeeded();
+	}
+
+	nudgeOut() {
+		this.perspectiveTransformer.multiplyScaleBy(1/1.01);
+		this.redrawNeeded();
+	}
+
+	redraw() {
+		if (this._redrawNeeded === true) {
+			const box = this.root.getBoundingClientRect();
+			this.canvas.setAttribute('width', box.width);
+			this.canvas.setAttribute('height', box.height);
+			this.perspectiveTransformer.setDimensions(box.width, box.height);
+			this._redrawNeeded = false;
+			let points = this.points;
+			if (this.rotatingTransformer !== undefined) {
+				const r = this.rotationRadius === undefined ? 200 : this.rotationRadius * 1.3;
+				this.perspectiveTransformer.position.assign(new Vector3D(0, 0, -r));
+				points = this.rotatingTransformer.getTransformedPoints(points);
+				sortPointCloudPoints(points);
+			}
+			drawPointCloud(this.canvas, this.perspectiveTransformer, points, this.backgroundColour);
+			this._lastRedrawTime = (new Date).getTime();
+			this._redrawNeeded = false;
+		}
+	}
+
+	redrawNeeded() {
+		this._redrawNeeded = true;
+		if (this._lastRedrawTime === undefined || (new Date).getTime() - this._lastRedrawTime > 30)
+			this.redraw();
+	}
+
+	setPoints(points) {
+		if (!(points instanceof Array))
+			throw new Error(`points must be an Array.  Not: ${points}`);
+		this.points = points;
+		this.backgroundColour = pointsToBackgroundColour(points);
+		updatePointCentre(this);
+		this.redrawNeeded();
+	}
+
+	setRotatingMode(mode) {
+		this.rotatingTransformer.setMode(mode);
+	}
+
+	updateDimensions() {
+		this.redrawNeeded();
+	}
+
+	zoomIn() {
+		this.perspectiveTransformer.multiplyScaleBy(1.1);
+		this.redrawNeeded();
+	}
+
+	zoomOut() {
+		this.perspectiveTransformer.multiplyScaleBy(1/1.1);
+		this.redrawNeeded();
+	}
+};
---
+++ turtle/turtle_test/modules/parsing/Command.js
@@ -0,0 +1,178 @@
+import { ArrayUtils } from '../ArrayUtils.js';
+import { DataTypes } from './data-types/DataTypes.js';
+import { fetchJson } from '../fetchJson.js';
+import { ParseTreeToken } from './ParseTreeToken.js';
+import { ParseTreeTokenType } from './ParseTreeTokenType.js';
+import { sanitizeCompositeValidation } from './sanitizeCompositeValidation.js';
+let commands;
+let commandMap;
+let hintCommandMap;
+
+async function asyncInit() {
+	commands = await fetchJson('json/commands.json');
+	await DataTypes.asyncInit();
+	commandMap = new Map();
+	hintCommandMap = new Map();
+
+	commands.forEach(function(commandInfo) {
+		commandMap.set(commandInfo.primaryName.toLowerCase(), commandInfo);
+		commandInfo.names.forEach(function(name) {
+			commandMap.set(name, commandInfo);
+		});
+		if (commandInfo.hintNames !== undefined)
+			commandInfo.hintNames.forEach(function(hintName) {
+				hintCommandMap.set(hintName, commandInfo);
+			});
+		commandInfo.args.forEach(function(argInfo) {
+			if (argInfo.format === 'stepPosition')
+				argInfo.sanitization = 'convertToStepPosition';
+		});
+		sanitizeCompositeValidation(commandInfo);
+	});
+}
+const initPromise = asyncInit();
+export class Command {
+	static asyncInit() {
+		return initPromise;
+	}
+
+	constructor(commandName, args) {
+		if (typeof commandName !== 'string')
+			throw new Error('commandName must be a string.');
+		if (!(args instanceof Array))
+			throw new Error('args must be an Array.');
+		args.forEach(function(arg, index) {
+			if (!(arg instanceof ParseTreeToken))
+				throw new Error('All elements of args must be ParseTreeToken.');
+		});
+
+		this.name = commandName;
+		this.args = args;
+	}
+
+	static getAllCommandsInfo() {
+		return commands;
+	}
+
+	static getArgCount(commandInfo) {
+		if (typeof commandInfo === 'string') {
+			const name = commandInfo;
+			commandInfo = Command.getCommandInfo(name);
+			if (commandInfo === undefined)
+				throw new Error('Unrecognized command: ' + name);
+		}
+		const result = {
+			'isFlexible': commandInfo.argCount === '?' || typeof commandInfo.argCount === 'object',
+			'defaultCount': commandInfo.args.length
+		};
+		if (typeof commandInfo.argCount === 'object') {
+			Object.assign(result, commandInfo.argCount);
+		}
+		return result;
+	}
+
+	static getCommandInfo(commandName) {
+		if (typeof commandName !== 'string')
+			throw new Error('getCommandInfo requires a string for commandName.  Not: ' + commandName);
+		commandName = commandName.toLowerCase();
+		if (commandMap.has(commandName))
+			return commandMap.get(commandName);
+	}
+
+	static getCommandInfoByHintName(hintName) {
+		if (typeof hintName !== 'string')
+			throw new Error('hintName must be a string.  Not: ' + hintName);
+
+		hintName = hintName.toLowerCase();
+		return hintCommandMap.get(hintName);
+	}
+
+	static getCommandsMatchingSearchKeywords(keywords) {
+		return commands.filter(function(info) {
+			if (info.searchKeywords === undefined)
+				return false;
+			for (let i = 0; i < keywords.length; i++) {
+				if (!info.searchKeywords.some(w => w === keywords[i])) {
+					return false;
+				}
+			}
+			return true;
+		});
+	}
+
+	static getCommandsWithFormat() {
+		return commands.filter(c => c.args.some(a => a.format !== undefined));
+	}
+
+	static getCommandsWithInstructionLists() {
+		return commands.filter(c => c.args.some(a => a.types === 'instructionlist'));
+	}
+
+	static getCommandsWithReadCommand() {
+		return commands.filter(c => c.readCommand !== undefined);
+	}
+
+	static getCommandsWithTypeEqualitySymbols() {
+		return commands.filter(c => c.args.some(a => a.typeEqualitySymbol !== undefined));
+	}
+
+	static getCommandsWithVariableRefTypes() {
+		return commands.filter(c => c.args.some(a => a.refTypes !== undefined));
+	}
+
+	static getLowerCaseCommandNameSet(commandInfo) {
+		if (typeof commandInfo === 'string') // command name instead of info object
+			commandInfo = Command.getCommandInfo(commandInfo);
+		const result = [commandInfo.primaryName.toLowerCase()];
+		ArrayUtils.pushAll(result, commandInfo.names);
+		return new Set(result);
+	}
+
+	static getMethodNameFor(commandInfo) {
+		return commandInfo.primaryName.replace(/\./g, '_');
+	}
+
+	static getParameterInfo(commandInfo, parameterIndex) {
+		if (parameterIndex >= 0 && parameterIndex < commandInfo.args.length)
+			return commandInfo.args[parameterIndex];
+		else
+			return {
+				'name': '_',
+				'types': '',
+			};
+	}
+
+	static getParameterName(commandInfo, parameterIndex) {
+		if (typeof commandInfo === 'string')
+			commandInfo = Command.getCommandInfo(commandInfo);
+		if (parameterIndex >= 0 && parameterIndex < commandInfo.args.length)
+			return commandInfo.args[parameterIndex].name;
+	}
+
+	static getParameterRefTypes(commandInfo, parameterIndex) {
+		if (typeof commandInfo === 'string')
+			commandInfo = Command.getCommandInfo(commandInfo);
+		if (parameterIndex >= 0 && parameterIndex < commandInfo.args.length)
+			return commandInfo.args[parameterIndex].refTypes;
+	}
+
+	static getParameterTypes(commandInfo, parameterIndex) {
+		if (typeof commandInfo === 'string')
+			commandInfo = Command.getCommandInfo(commandInfo);
+		if (parameterIndex >= 0 && parameterIndex < commandInfo.args.length)
+			return commandInfo.args[parameterIndex].types;
+		else if (commandInfo.extraArgsInfo !== undefined)
+			return commandInfo.extraArgsInfo.types;
+		else
+			return DataTypes.getAllTypesString(); // return all types
+	}
+
+	static getReturnDataTypes(commandInfo) {
+		return new DataTypes(commandInfo.returnTypes);
+	}
+
+	static getReturnTypes(commandInfo) {
+		return commandInfo.returnTypes;
+	}
+};
+
