3c5cd6426b7f8c890d38ff040865aedb9dad05b6
Josh Greig <josh.greig@gmail.com>
2024-01-28 03:39:55 -0500

    turtle: added turtle_test

---
+++ turtle/turtle_test/modules/parsing/compiling/instruction-list-optimization/instructions-to-JavaScript/processPushCluster.js
@@ -0,0 +1,46 @@
+import { JavaScriptInstruction } from '../../../execution/instructions/JavaScriptInstruction.js';
+import { PushInstruction } from '../../../execution/instructions/PushInstruction.js';
+import { removeInstructions } from '../removeInstructions.js';
+import { Transparent } from '../../../../Transparent.js';
+import { valueToJavaScript } from './valueToJavaScript.js';
+import { VariableReadInstruction } from '../../../execution/instructions/VariableReadInstruction.js';
+
+function getStopIndex(instructions, startIndex) {
+	for (let i = startIndex; i >= 0; i--) {
+		const instruction = instructions[i];
+		if (!isPushClusterInstruction(instruction))
+			return i + 1;
+	}
+	return startIndex;
+}
+
+export function getExpressionFrom(instruction, isLocal) {
+	if (instruction instanceof PushInstruction) {
+		return valueToJavaScript(instruction.value, 20);
+	}
+	else {
+		if (isLocal === true)
+			return `context.getCurrentExecutingProcedure().localVariables.get("${instruction.variableName}")`;
+		else if (isLocal === false)
+			return `context.globalVariables.get("${instruction.variableName}")`;
+		else
+			return `context.readVariable("${instruction.variableName}")`;
+	}
+};
+
+export function isPushClusterInstruction(instruction) {
+	return instruction instanceof PushInstruction ||
+		instruction instanceof VariableReadInstruction;
+};
+
+export function processPushCluster(instructions, index) {
+	const stopIndex = getStopIndex(instructions, index);
+	let expressions = [];
+	for (let i = stopIndex; i <= index; i++) {
+		expressions.push(getExpressionFrom(instructions[i]));
+	}
+	const code = 'context.valueStack.push(' + expressions.join(',') + ')';
+	instructions[stopIndex] = new JavaScriptInstruction(code, instructions[stopIndex].parseTreeToken);
+	removeInstructions(instructions, stopIndex + 1, index - stopIndex);
+	return stopIndex;
+};
