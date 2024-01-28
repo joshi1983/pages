520965d2c327e18355936ef6d9afe8f0314a1f03
Josh Greig <josh.greig@gmail.com>
2024-01-28 04:15:54 -0500

    turtle: improved turtle_test removed downscale worker

--- turtle/turtle_test/modules/drawing-menu/download/drawing-download/previewerDownload.js
+++ turtle/turtle_test/modules/drawing-menu/download/drawing-download/previewerDownload.js
@@ -1,4 +1,3 @@
-import { asyncDownscale } from '../../../drawing/drawers/canvas/asyncDownscale.js';
 import { clamp } from '../../../clamp.js';
 import { CanvasVector2DDrawer } from '../../../drawing/drawers/CanvasVector2DDrawer.js';
 import { downloadDataUrl } from '../../../components/downloadDataUrl.js';
@@ -18,11 +17,6 @@ function getSVGDataURL(drawing, transformer) {
 	return 'data:image/svg+xml;base64,'+btoa(text);
 }
 
-function getDownscaleFactor(transformer) {
-	const pixelCount = transformer.width * transformer.height;
-	return Math.floor(clamp(Math.sqrt(5000000 / pixelCount), 1, 8));
-}
-
 async function getRasterDataURL(drawing, transformer, mime) {
 	function createCanvas() {
 		const result = document.createElement('canvas');
@@ -33,30 +27,12 @@ async function getRasterDataURL(drawing, transformer, mime) {
 	let singleLayerCanvas = createCanvas();
 	const originalWidth = drawing.width;
 	const originalHeight = drawing.height;
-	const downscaleFactor = getDownscaleFactor(transformer);
 
-	if (downscaleFactor === 1) {
-		drawing.width = transformer.width;
-		drawing.height = transformer.height;
-		const drawer = new CanvasVector2DDrawer(undefined, transformer.width, transformer.height);
-		drawing.drawAsSingleLayer(drawer, transformer.toCamera());
-		drawer.copyToSingleCanvas(singleLayerCanvas);
-	}
-	else {
-		const downscaleWidth = transformer.width * downscaleFactor;
-		const downscaleHeight = transformer.height * downscaleFactor;
-		transformer.setDimensions(downscaleWidth, downscaleHeight);
-		transformer.setScale(transformer.scale * downscaleFactor);
-		const drawer = new CanvasVector2DDrawer(undefined, transformer.width, transformer.height);
-		drawing.width = transformer.width;
-		drawing.height = transformer.height;
-		const largeCanvas = createCanvas();
-		drawing.drawAsSingleLayer(drawer, transformer.toCamera());
-		drawer.copyToSingleCanvas(largeCanvas);
-		const imageData = await asyncDownscale(downscaleFactor, largeCanvas);
-		const ctx = singleLayerCanvas.getContext('2d');
-		ctx.putImageData(imageData, 0, 0);
-	}
+	drawing.width = transformer.width;
+	drawing.height = transformer.height;
+	const drawer = new CanvasVector2DDrawer(undefined, transformer.width, transformer.height);
+	drawing.drawAsSingleLayer(drawer, transformer.toCamera());
+	drawer.copyToSingleCanvas(singleLayerCanvas);
 
 	// restore original dimensions of drawing.
 	// If the dimensions aren't restored, downloading an image with 
