bc728098b6d9939ff65c5fa5fdad08aa2435ac7f
Josh Greig <josh.greig@gmail.com>
2024-01-28 03:39:55 -0500

    turtle: added turtle_test

---
+++ turtle/turtle_test/modules/drawing/drawers/svg/drawLine.js
@@ -0,0 +1,27 @@
+import { lineToCircle } from './lineToCircle.js';
+
+export function drawLine(svgDrawer, line) {
+	const rounder = svgDrawer.rounder;
+	const pos = line.position;
+	const pos2 = line.endPoint;
+	if (pos.equals(pos2) && line.style.getLineCap() === 'round') {
+		svgDrawer.drawCircle(lineToCircle(line));
+		return;
+	}
+	const attrMap = new Map([
+		['x1', pos.getX()],
+		['y1', pos.getY()],
+		['x2', pos2.getX()],
+		['y2', pos2.getY()]
+	]);
+	let coords = '';
+	for (const [key, val] of attrMap) {
+		const formattedVal = rounder.formatNumber(val);
+		if (formattedVal !== '0') {
+			// no need to specify the attribute unless it is not 0
+			// since x1,y1,x2,y2 have 0 as their default values.
+			coords += ` ${key}="${formattedVal}"`;
+		}
+	}
+	svgDrawer.pushTag('<line' + coords + svgDrawer.getStrokeStyle(line.style) + ' />');
+};
---
+++ turtle/turtle_test/modules/file/assets/asset-viewers/PlainTextAssetViewer.js
@@ -0,0 +1,25 @@
+import { AbstractAssetViewer } from './AbstractAssetViewer.js';
+const textExtensions = new Set([
+'css','htm', 'html', 'ini', 'js', 'json', 'lgo',
+'rss', 'txt', 'xml', 'xsl']);
+
+export class PlainTextAssetViewer extends AbstractAssetViewer {
+	constructor(asset) {
+		super(asset);
+	}
+
+	getDiv() {
+		if (this.div === undefined) {
+			this.div = document.createElement('div');
+			this.div.classList.add('text-asset-view');
+			const pre = document.createElement('pre');
+			pre.innerText = this.asset.getDataAsString().trim();
+			this.div.appendChild(pre);
+		}
+		return this.div;
+	}
+
+	static matchesExtension(extension) {
+		return textExtensions.has(extension);
+	}
+};
