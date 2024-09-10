879cf962cfaa6edf251e919d52a298b73283f1c0
Josh Greig <josh.greig@gmail.com>
2024-09-09 23:47:30 -0400

    turtle: started using OptionsList

--- turtle/modules/components/OptionList.js
+++ turtle/modules/components/OptionList.js
@@ -1,22 +1,90 @@
-export class OptionList {
+import { EventDispatcher } from '../EventDispatcher.js';
+
+function getInitialIndex(optionsInfo) {
+	let result = -1;
+	optionsInfo.forEach(function(optionInfo, index) {
+		if (optionInfo.selected === true)
+			result = index;
+	});
+	if (result === -1)
+		result = 0;
+	return result;
+}
+
+export class OptionList extends EventDispatcher {
 	constructor(id, optionsInfo) {
 		if (typeof id !== 'string')
 			throw new Error(`id must be a string but got ${id}`);
 		if (!(optionsInfo instanceof Array))
 			throw new Error(`optionsInfo must be an Array but got ${optionsInfo}`);
+		super(['selection-changed']);
 		const e = document.getElementById(id);
-		item.e = e;
-		if (this.e === null)
+		if (e === null)
 			throw new Error(`Unable to find element with id ${id}`);
-		optionsInfo.forEach(function(optionInfo) {
+		const selectedIndex = getInitialIndex(optionsInfo);
+		this.e = e;
+		this.selectedOption;
+		this.itemsInfo = optionsInfo;
+		this.items = [];
+		const outer = this;
+		optionsInfo.forEach(function(optionInfo, index) {
 			const item = document.createElement('a');
 			item.innerText = optionInfo.text;
-			item.addEventListener('click', function() {
-				
+			if (optionInfo.value !== undefined)
+				item.dataset.val = optionInfo.value;
+			if (selectedIndex === index) {
+				item.classList.add('selected');
+				outer.selectedOption = item;
+			}
+			item.addEventListener('click', function(event) {
+				const option = event.target;
+				if (option === outer.selectedOption)
+					return; // no change happened.
+				const index = outer.items.indexOf(option);
+				outer._selectedIndex(index);
 			});
+			outer.items.push(item);
 			e.appendChild(item);
 		});
 	}
 
-	
+	_selectedIndex(index_) {
+		const selectedItem = this.items[index_];
+		if (selectedItem === undefined)
+			throw new Error(`Unable to select an item at index ${index_}.`);
+		if (!selectedItem.classList.contains('selected')) {
+			this._unselectAll();
+			selectedItem.classList.add('selected');
+			this.selectedOption = selectedItem;
+			this._dispatchEvent('selection-changed', {
+				'element': selectedItem,
+				'optionInfo': this.itemsInfo[index_]
+			});
+		}
+	}
+
+	_unselectAll() {
+		this.items.forEach(function(item) {
+			item.classList.remove('selected');
+		});
+	}
+
+	dispose() {
+		this.items.forEach(function(item) {
+			item.remove();
+		});
+	}
+
+	setSelectedInnerText(innerText) {
+		let index_;
+		this.items.forEach(function(e, index) {
+			if (e.innerText === innerText) {
+				index_ = index;
+			}
+		});
+		const selectedItem = this.items[index_];
+		if (selectedItem === undefined)
+			throw new Error(`Unable to find an option with innerText "${innerText}"`);
+		this._selectedIndex(index_);
+	}
 };
--- turtle/tests/components/testComponents.js
+++ turtle/tests/components/testComponents.js
@@ -1,4 +1,4 @@
-import { testAsyncCachedMap } from './testAsyncCachedMap.js';
+/*import { testAsyncCachedMap } from './testAsyncCachedMap.js';
 import { testCodeEditor } from './code-editor/testCodeEditor.js';
 import { testDialog } from './dialog/testDialog.js';
 import { testElementResizeListener } from './testElementResizeListener.js';
@@ -7,17 +7,18 @@ import { testGetFileFormatFromArrayBuffer } from './testGetFileFormatFromArrayBu
 import { testGetProcedureInfo } from './testGetProcedureInfo.js';
 import { testGlossaryRepository } from './testGlossaryRepository.js';
 import { testMessageToDivNoProcessLinks } from './testMessageToDivNoProcessLinks.js';
-import { testPopStateListener } from './testPopStateListener.js';
+*/import { testOptionList } from './testOptionList.js';
+/*import { testPopStateListener } from './testPopStateListener.js';
 import { testPushStates } from './testPushStates.js';
 import { testSelectionUtils } from './testSelectionUtils.js';
 import { testSVGDrawingViewerDirectory } from './svg-drawing-viewer/testSVGDrawingViewerDirectory.js';
 import { testSyntaxHighlighter } from './syntax-highlighter/testSyntaxHighlighter.js';
 import { testUpdateHexColourDisplay } from './testUpdateHexColourDisplay.js';
-import { wrapAndCall } from '../helpers/wrapAndCall.js';
+*/import { wrapAndCall } from '../helpers/wrapAndCall.js';
 
 export function testComponents(logger) {
 	wrapAndCall([
-		testAsyncCachedMap,
+		/*testAsyncCachedMap,
 		testCodeEditor,
 		testDialog,
 		testElementResizeListener,
@@ -26,11 +27,12 @@ export function testComponents(logger) {
 		testGetProcedureInfo,
 		testGlossaryRepository,
 		testMessageToDivNoProcessLinks,
-		testPopStateListener,
+		*/testOptionList,
+		/*testPopStateListener,
 		testPushStates,
 		testSelectionUtils,
 		testSVGDrawingViewerDirectory,
 		testSyntaxHighlighter,
-		testUpdateHexColourDisplay
+		testUpdateHexColourDisplay*/
 	], logger);
 };
