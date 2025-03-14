15498a2c11a889547804c3f3c6ce2893baf19cb9
Josh Greig <josh.greig@gmail.com>
2025-03-14 11:16:10 -0400

    turtle: improved sort wip

--- turtle/tests/parsing/execution/testLogoProgramExecuterInternalProcedures.js
+++ turtle/tests/parsing/execution/testLogoProgramExecuterInternalProcedures.js
@@ -9,6 +9,10 @@ export function testLogoProgramExecuterInternalProcedures(logger) {
 			print map "mulTwo [1 2 5]`,
 			'messages': ['[2 4 10]']
 		},
+		{
+			'code': 'print (sort2 [1 -2 3] "less? )'
+			'messages': ['[-2 1 3]']
+		},
 	];
 	processExecuterTestCases(cases, logger);
 };
