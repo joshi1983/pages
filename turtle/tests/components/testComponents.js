e2fd420bb6bf9541c199a0c5d9f2b9642ed277cc
Josh Greig <josh.greig@gmail.com>
2025-05-07 20:28:41 -0400

    turtle: improved wip

--- turtle/tests/components/testComponents.js
+++ turtle/tests/components/testComponents.js
@@ -1,4 +1,4 @@
-import { testAsyncCachedMap } from './testAsyncCachedMap.js';
+/*import { testAsyncCachedMap } from './testAsyncCachedMap.js';
 import { testCodeEditor } from './code-editor/testCodeEditor.js';
 import { testDialog } from './dialog/testDialog.js';
 import { testElementResizeListener } from './testElementResizeListener.js';
@@ -6,33 +6,33 @@ import { testEventQueue } from './testEventQueue.js';
 import { testGetFileFormatFromArrayBuffer } from './testGetFileFormatFromArrayBuffer.js';
 import { testGetProcedureInfo } from './testGetProcedureInfo.js';
 import { testGlossaryRepository } from './testGlossaryRepository.js';
-import { testImageFormats } from './image-formats/testImageFormats.js';
-import { testMessageToDivNoProcessLinks } from './testMessageToDivNoProcessLinks.js';
+*/import { testImageFormats } from './image-formats/testImageFormats.js';
+/*import { testMessageToDivNoProcessLinks } from './testMessageToDivNoProcessLinks.js';
 import { testPopStateListener } from './testPopStateListener.js';
 import { testPushStates } from './testPushStates.js';
 import { testSelectionUtils } from './testSelectionUtils.js';
 import { testSVGDrawingViewerDirectory } from './svg-drawing-viewer/testSVGDrawingViewerDirectory.js';
 import { testSyntaxHighlighter } from './syntax-highlighter/testSyntaxHighlighter.js';
 import { testUpdateHexColourDisplay } from './testUpdateHexColourDisplay.js';
-import { wrapAndCall } from '../helpers/wrapAndCall.js';
+*/import { wrapAndCall } from '../helpers/wrapAndCall.js';
 
 export function testComponents(logger) {
 	wrapAndCall([
-		testAsyncCachedMap,
+		/*testAsyncCachedMap,
 		testCodeEditor,
 		testDialog,
 		testElementResizeListener,
 		testEventQueue,
 		testGetFileFormatFromArrayBuffer,
 		testGetProcedureInfo,
-		testGlossaryRepository,
+		testGlossaryRepository,*/
 		testImageFormats,
-		testMessageToDivNoProcessLinks,
+		/*testMessageToDivNoProcessLinks,
 		testPopStateListener,
 		testPushStates,
 		testSelectionUtils,
 		testSVGDrawingViewerDirectory,
 		testSyntaxHighlighter,
-		testUpdateHexColourDisplay
+		testUpdateHexColourDisplay*/
 	], logger);
 };
