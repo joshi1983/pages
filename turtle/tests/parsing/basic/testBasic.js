f3a45cdb01674b04b14b820319d0929e79f32c85
Josh Greig <josh.greig@gmail.com>
2025-07-25 02:20:09 -0400

    turtle: improved wip

--- turtle/tests/parsing/basic/testBasic.js
+++ turtle/tests/parsing/basic/testBasic.js
@@ -1,25 +1,25 @@
-import { testAppleSoftBasic } from
+/*import { testAppleSoftBasic } from
 './applesoft-basic/testAppleSoftBasic.js';
-import { testBBCBasic } from
+*/import { testBBCBasic } from
 './bbc-basic/testBBCBasic.js';
 import { testHelpers } from
 './helpers/testHelpers.js';
 import { testQBasic } from
 './qbasic/testQBasic.js';
-import { testSmallVisualBasic } from
+/*import { testSmallVisualBasic } from
 './small-visual-basic/testSmallVisualBasic.js';
 import { testTektronix405xBasic } from
 './tektronix-405x-basic/testTektronix405xBasic.js';
-import { wrapAndCall } from 
+*/import { wrapAndCall } from 
 '../../helpers/wrapAndCall.js';
 
 export function testBasic(logger) {
 	wrapAndCall([
-		testAppleSoftBasic,
-		testBBCBasic,
+		/*testAppleSoftBasic,
+		*/testBBCBasic,
 		testHelpers,
 		testQBasic,
-		testSmallVisualBasic,
-		testTektronix405xBasic
+		/*testSmallVisualBasic,
+		testTektronix405xBasic*/
 	], logger);
 };
