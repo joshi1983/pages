772616b7d03f32de9026b3d3b1884f4a541215c0
Josh Greig <josh.greig@gmail.com>
2025-08-30 23:32:28 -0400

    turtle: improved wip

--- turtle/tests/components/testComponents.js
+++ turtle/tests/components/testComponents.js
@@ -1,4 +1,4 @@
-import { testAsyncCachedMap } from './testAsyncCachedMap.js';
+/*import { testAsyncCachedMap } from './testAsyncCachedMap.js';
 import { testCodeEditor } from './code-editor/testCodeEditor.js';
 import { testDialog } from './dialog/testDialog.js';
 import { testElementResizeListener } from './testElementResizeListener.js';
@@ -6,8 +6,8 @@ import { testEventQueue } from './testEventQueue.js';
 import { testGetFileFormatFromArrayBuffer } from './testGetFileFormatFromArrayBuffer.js';
 import { testGetProcedureInfo } from './testGetProcedureInfo.js';
 import { testGlossaryRepository } from './testGlossaryRepository.js';
-import { testImageFormats } from './image-formats/testImageFormats.js';
-import { testMessageToDivNoProcessLinks } from './testMessageToDivNoProcessLinks.js';
+*/import { testImageFormats } from './image-formats/testImageFormats.js';
+/*import { testMessageToDivNoProcessLinks } from './testMessageToDivNoProcessLinks.js';
 import { testPopStateListener } from './testPopStateListener.js';
 import { testPushStates } from './testPushStates.js';
 import { testSelectionUtils } from './testSelectionUtils.js';
@@ -15,11 +15,11 @@ import { testSVGDrawingViewerDirectory } from './svg-drawing-viewer/testSVGDrawi
 import { testSyntaxHighlighter } from './syntax-highlighter/testSyntaxHighlighter.js';
 import { testUpdateHexColourDisplay } from './testUpdateHexColourDisplay.js';
 import { testURLUtils } from './testURLUtils.js';
-import { wrapAndCall } from '../helpers/wrapAndCall.js';
+*/import { wrapAndCall } from '../helpers/wrapAndCall.js';
 
 export function testComponents(logger) {
 	wrapAndCall([
-		testAsyncCachedMap,
+		/*testAsyncCachedMap,
 		testCodeEditor,
 		testDialog,
 		testElementResizeListener,
@@ -27,14 +27,14 @@ export function testComponents(logger) {
 		testGetFileFormatFromArrayBuffer,
 		testGetProcedureInfo,
 		testGlossaryRepository,
-		testImageFormats,
-		testMessageToDivNoProcessLinks,
+		*/testImageFormats,
+		/*testMessageToDivNoProcessLinks,
 		testPopStateListener,
 		testPushStates,
 		testSelectionUtils,
 		testSVGDrawingViewerDirectory,
 		testSyntaxHighlighter,
 		testUpdateHexColourDisplay,
-		testURLUtils
+		testURLUtils*/
 	], logger);
 };
