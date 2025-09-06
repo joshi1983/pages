a430d020213aa4e02a2d55758fe9dd0b84bea094
Josh Greig <josh.greig@gmail.com>
2025-09-06 16:02:56 -0400

    turtle: improved wip

--- turtle/modules/parsing/python-parsing/loadDTParser.js
+++ turtle/modules/parsing/python-parsing/loadDTParser.js
@@ -2,4 +2,7 @@ import { loadJavaScript } from '../../components/loadJavaScript.js';
 
 export function loadDTParser() {
 	return loadJavaScript('lib/dt-python-parser/dt-python-parser.min.js');
+	//return loadJavaScript('lib/dt-python-parser/dt-python-parser.js');
+	//return loadJavaScript('lib/dt-python-parser/dt-python-parser-source-troubleshooting.js');
+	//return loadJavaScript('lib/dt-python-parser/dt-python-parser.min-troubleshooting.js');
 };
