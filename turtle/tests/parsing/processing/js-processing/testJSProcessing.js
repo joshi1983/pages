d572c2c74b1801aafb63eeff584a7748ffb33c93
Josh Greig <josh.greig@gmail.com>
2025-11-25 23:18:51 -0600

    turtle: improved wip

--- turtle/tests/parsing/processing/js-processing/testJSProcessing.js
+++ turtle/tests/parsing/processing/js-processing/testJSProcessing.js
@@ -1,14 +1,14 @@
-import { testFunctionsJSON } from './testFunctionsJSON.js';
+/*import { testFunctionsJSON } from './testFunctionsJSON.js';
 import { testIsLikelyJavaScriptProcessing } from './testIsLikelyJavaScriptProcessing.js';
 import { testParseAllJSProcessingExamples } from './testParseAllJSProcessingExamples.js';
-import { testTranslationToWebLogo } from './translation-to-weblogo/testTranslationToWebLogo.js';
+*/import { testTranslationToWebLogo } from './translation-to-weblogo/testTranslationToWebLogo.js';
 import { wrapAndCall } from '../../../helpers/wrapAndCall.js';
 
 export function testJSProcessing(logger) {
 	wrapAndCall([
-		testFunctionsJSON,
+		/*testFunctionsJSON,
 		testIsLikelyJavaScriptProcessing,
 		testParseAllJSProcessingExamples,
-		testTranslationToWebLogo
+		*/testTranslationToWebLogo
 	], logger);
 };
