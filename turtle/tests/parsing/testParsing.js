8e7d6dd9f34939446d34013c9e2018516ba02ea2
Josh Greig <josh.greig@gmail.com>
2026-01-03 10:37:39 -0600

    turtle: improved wip

--- turtle/tests/parsing/testParsing.js
+++ turtle/tests/parsing/testParsing.js
@@ -1,4 +1,4 @@
-import { testASMTurtle } from './asm-turtle/testASMTurtle.js';
+/*import { testASMTurtle } from './asm-turtle/testASMTurtle.js';
 import { testAsyncParser } from './testAsyncParser.js';
 import { testAsyncParseTask } from './testAsyncParseTask.js';
 import { testBasic } from './basic/testBasic.js';
@@ -33,8 +33,8 @@ import { testMightBeRunnableCode } from './testMightBeRunnableCode.js';
 import { testOperators } from './testOperators.js';
 import { testOrderOfOperation } from './testOrderOfOperation.js';
 import { testParseTree } from './testParseTree.js';
-import { testParseTreeAnalysis } from './parse-tree-analysis/testParseTreeAnalysis.js';
-import { testParseTreeColIndexes } from './testParseTreeColIndexes.js';
+*/import { testParseTreeAnalysis } from './parse-tree-analysis/testParseTreeAnalysis.js';
+/*import { testParseTreeColIndexes } from './testParseTreeColIndexes.js';
 import { testParseTreeToCodeWithComments } from './testParseTreeToCodeWithComments.js';
 import { testParseTreeToCodeWithCommentsInsertSpace } from './testParseTreeToCodeWithCommentsInsertSpace.js';
 import { testParseTreeToken } from './testParseTreeToken.js';
@@ -52,11 +52,11 @@ import { testSonicWebTurtle } from './sonic-webturtle/testSonicWebTurtle.js';
 import { testSugarLabsTurtleBlocks } from './sugarlabs-turtle-blocks/testSugarLabsTurtleBlocks.js';
 import { testToken } from './testToken.js';
 import { testUnsupportedCommand } from './testUnsupportedCommand.js';
-import { wrapAndCall } from '../helpers/wrapAndCall.js';
+*/import { wrapAndCall } from '../helpers/wrapAndCall.js';
 
 export function testParsing(logger) {
 	wrapAndCall([
-		testASMTurtle,
+		/*testASMTurtle,
 		testAsyncParser,
 		testAsyncParseTask,
 		testBasic,
@@ -90,9 +90,9 @@ export function testParsing(logger) {
 		testMightBeRunnableCode,
 		testOperators,
 		testOrderOfOperation,
-		testParseTree,
+		testParseTree,*/
 		testParseTreeAnalysis,
-		testParseTreeColIndexes,
+		/*testParseTreeColIndexes,
 		testParseTreeToCodeWithComments,
 		testParseTreeToCodeWithCommentsInsertSpace,
 		testParseTreeToken,
@@ -109,6 +109,6 @@ export function testParsing(logger) {
 		testSonicWebTurtle,
 		testSugarLabsTurtleBlocks,
 		testToken,
-		testUnsupportedCommand
+		testUnsupportedCommand*/
 	], logger);
 };
