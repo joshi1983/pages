8b3428bb18402720696c7f2e82486044fc776fcd
Josh Greig <josh.greig@gmail.com>
2026-02-01 16:13:57 -0600

    mask-blaster: improved

--- mask-blaster/modules/screens/controlsScreen.js
+++ mask-blaster/modules/screens/controlsScreen.js
@@ -1,9 +1,24 @@
 import { addListener, Modes, setMode } from '../Modes.js';
+import { ready } from '../ready.js';
 
 let button;
+let mouseUsed = false, touchUsed = false;
+
+function shouldGoTouchOnly() {
+	if (mouseUsed === false && touchUsed === false)
+		return false;
+	if (mouseUsed)
+		return false;
+	return true;
+}
 
 addListener(function(newMode) {
 	if (newMode === Modes.CONTROLS) {
+		const screenElement = document.getElementById('controls-screen');
+		if (shouldGoTouchOnly()) {
+			screenElement.classList.add('touch-only-selected');
+		}
+		
 		if (button === undefined) {
 			button = document.getElementById('startGameLoop');
 			button.addEventListener('click', function() {
@@ -13,3 +28,17 @@ addListener(function(newMode) {
 		button.focus(); // for easier control using a keyboard
 	}
 });
+
+function handleTouchEvent() {
+	touchUsed = true;
+}
+
+function init() {
+	window.addEventListener('mousedown', function(event) {
+		mouseUsed = true;
+	});
+	window.addEventListener('touchstart', handleTouchEvent);
+	window.addEventListener('touchend', handleTouchEvent);
+}
+
+ready(init);
