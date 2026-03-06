3e4fee19041b6569df152dffa990b03cd82a611a
Josh Greig <josh.greig@gmail.com>
2026-03-05 18:25:32 -0600

    turtle: improved wip

--- turtle/tests/parsing/pitrified-go-turtle/scanning/testScanning.js
+++ turtle/tests/parsing/pitrified-go-turtle/scanning/testScanning.js
@@ -1,26 +1,26 @@
-import { testIsCompleteCharacterLiteral } from './testIsCompleteCharacterLiteral.js';
+/*import { testIsCompleteCharacterLiteral } from './testIsCompleteCharacterLiteral.js';
 import { testIsCompleteNumberLiteral } from './testIsCompleteNumberLiteral.js';
 import { testIsCompleteStringLiteral } from './testIsCompleteStringLiteral.js';
 import { testIsFloatingPointLiteralStart } from './testIsFloatingPointLiteralStart.js';
-import { testIsIdentifier } from './testIsIdentifier.js';
-import { testIsImaginaryNumberLiteral } from './testIsImaginaryNumberLiteral.js';
+*/import { testIsIdentifier } from './testIsIdentifier.js';
+/*import { testIsImaginaryNumberLiteral } from './testIsImaginaryNumberLiteral.js';
 import { testIsMarkingEndOfToken } from './testIsMarkingEndOfToken.js';
 import { testScan } from './testScan.js';
 import { testScanValidNumberLiterals } from './testScanValidNumberLiterals.js';
 import { testScanVariousExamples } from './testScanVariousExamples.js';
-import { wrapAndCall } from '../../../helpers/wrapAndCall.js';
+*/import { wrapAndCall } from '../../../helpers/wrapAndCall.js';
 
 export function testScanning(logger) {
-	wrapAndCall([
+	wrapAndCall([/*
 		testIsCompleteCharacterLiteral,
 		testIsCompleteNumberLiteral,
-		testIsCompleteStringLiteral,
+		testIsCompleteStringLiteral,*/
 		testIsIdentifier,
-		testIsImaginaryNumberLiteral,
+		/*testIsImaginaryNumberLiteral,
 		testIsMarkingEndOfToken,
 		testIsFloatingPointLiteralStart,
 		testScan,
 		testScanValidNumberLiterals,
-		testScanVariousExamples,
+		testScanVariousExamples,*/
 	], logger);
 };
