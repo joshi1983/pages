1fae4787b3d6fc8b5d252c37a6d3779c29b704c2
Josh Greig <josh.greig@gmail.com>
2026-04-09 05:58:34 -0500

    turtle: improved wip

--- turtle/tests/test.js
+++ turtle/tests/test.js
@@ -12,7 +12,7 @@ import { StringUtils } from
 '../modules/StringUtils.js';
 
 let testModuleUrls = [
-	'./bindDocumentFocusTests.js',
+	/*'./bindDocumentFocusTests.js',
 	'./testAlphaColour.js',
 	'./testArrayUtils.js',
 	'./assets/testAssets.js',
@@ -44,8 +44,8 @@ let testModuleUrls = [
 	'./testLongestCommonSubsequence.js',
 	'./testMapUtils.js',
 	'./testMaybeDecided.js',
-	'./testOperatorsJSON.js',
-	'./parsing/testParsing.js',
+	'./testOperatorsJSON.js',*/
+	'./parsing/testParsing.js',/*
 	'./testPopulateTemplateUsingObject.js',
 	'./testRateLimiter.js',
 	'./set/testSet.js',
@@ -57,7 +57,7 @@ let testModuleUrls = [
 	'./testValueToLiteralCode.js',
 	'./testValueToString.js',
 	'./testValueWrapper.js',
-	'./testWindowsCommandsJSON.js'
+	'./testWindowsCommandsJSON.js'*/
 ];
 
 function processSearch() {
