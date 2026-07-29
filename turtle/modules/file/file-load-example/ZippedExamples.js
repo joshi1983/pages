c8fb804d4a84e82a48e395b35792e23c2be75610
Josh Greig <josh.greig@gmail.com>
2026-07-28 22:43:31 -0500

    turtle: zip

---
+++ turtle/modules/file/file-load-example/ZippedExamples.js
@@ -0,0 +1,33 @@
+let examplesData = new Map();
+let promise = fetch('dist/scriptExamples.zip?t=' + Date.now()).
+	then(response => response.blob()).
+	then(function(blob) {
+		var zip = new window.JSZip();
+		const promises = [];
+		return zip.loadAsync(blob).then(function(contents) {
+			for (const key in contents.files) {
+				const file = zip.file(key);
+				promises.push(file.async('text').then(function(textContent) {
+					examplesData.set(key, textContent);
+				}));
+			}
+		}).then(function() {
+			return Promise.all(promises);
+		});
+	}).catch(function(e) {
+		console.error(`catch: ${e}`);
+	});
+
+export class ZippedExamples {
+	static asyncInit() {
+		return promise;
+	}
+
+	static getFilenames() {
+		return Array.from(examplesData.keys());
+	}
+
+	static getContentForFilename(filename) {
+		return examplesData.get(filename);
+	}
+};
